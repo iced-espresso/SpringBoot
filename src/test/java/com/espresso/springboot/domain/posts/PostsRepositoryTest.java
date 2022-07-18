60cabe9f272bea44fe39cda73a1e44edb0cdd884
iced-espresso <csu2018@gmail.com>
2022-07-18 23:59:36 +0900

    add test code for PostsRepository
    application.properties 설정하여 MySQL 버전으로 쿼리 로그 출력
    Signed-off-by: iced-espresso <csu2018@gmail.com>

---
+++ src/test/java/com/espresso/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.espresso.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시글 제목";
+        String content = "테스트 게시글 본문";
+        String author = "csu2018@gmail.com";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getAuthor()).isEqualTo(author);
+    }
+
+}
