c7d94d153847f19b4348e5f9f33efb755eec4c99
iced-espresso <csu2018@gmail.com>
2022-07-19 23:23:10 +0900

    add code for PostsService/PostsApiController/PostsSaveRequestDto
    - 게시글 등록 코드 추가
    
    Signed-off-by: iced-espresso <csu2018@gmail.com>

--- src/main/java/com/espresso/springboot/service/posts/PostsService.java
+++ src/main/java/com/espresso/springboot/service/posts/PostsService.java
@@ -1,4 +1,18 @@
 package com.espresso.springboot.service.posts;
 
+import com.espresso.springboot.domain.posts.PostsRepository;
+import com.espresso.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
