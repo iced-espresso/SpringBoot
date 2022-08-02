2484133e0cb77866ed307c693322dc8e2d007198
iced-espresso <csu2018@gmail.com>
2022-08-02 22:22:04 +0900

    IndexControllerTest 기능 추가

---
+++ src/test/java/com/espresso/springboot/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.espresso.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩(){
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/", String.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        String body = responseEntity.getBody();
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
