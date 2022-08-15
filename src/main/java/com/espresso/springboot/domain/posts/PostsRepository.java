9c5df67852f4844f2434df8ad63fd65a52abdc80
iced-espresso <csu2018@gmail.com>
2022-08-16 02:11:03 +0900

    - mysql로 db 대체 - test 시에는 inmemory db 사용하도록 test의 application.properties 분리

--- src/main/java/com/espresso/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/espresso/springboot/domain/posts/PostsRepository.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
-    @Query("SELECT p FROM Posts p Order by p.modifiedDate DESC")
+    @Query("SELECT p FROM Posts p Order by p.modifiedDate DESC, p.id DESC")
     List<Posts> findAllDesc();
 
-    @Query("SELECT p FROM Posts p Order by p.modifiedDate DESC")
+    @Query("SELECT p FROM Posts p Order by p.modifiedDate DESC, p.id DESC")
     List<Posts> findAllDesc(Pageable pageable);
 
 }
