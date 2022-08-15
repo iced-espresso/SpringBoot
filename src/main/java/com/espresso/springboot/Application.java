721bc7927bd285a04e41b51a5ed7a3b57f7406cb
iced-espresso <csu2018@gmail.com>
2022-08-16 02:14:50 +0900

    - front 문구 변경 - JpaAuditingConfiguration 파일로 뺌(fix for HelloControllerTest error)

--- src/main/java/com/espresso/springboot/Application.java
+++ src/main/java/com/espresso/springboot/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application implements WebMvcConfigurer {
     public static void main(String args[]){
---
+++ src/main/java/com/espresso/springboot/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.espresso.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
