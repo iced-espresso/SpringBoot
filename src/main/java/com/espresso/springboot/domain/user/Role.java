17372c76a13a4a11c612af515a7086cd76bf0d58
iced-espresso <csu2018@gmail.com>
2022-08-18 02:06:50 +0900

    - User 관련 클래스 추가(User/Role/UserRepository) - build.gradle에 oauth2-client 의존성 추가

---
+++ src/main/java/com/espresso/springboot/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.espresso.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST", "게스트"),
+    USER("ROLE_USER", "유저"),
+    ;
+
+    private final String key;
+    private final String title;
+}
