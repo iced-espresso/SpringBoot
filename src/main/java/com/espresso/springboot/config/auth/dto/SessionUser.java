3b6533b3df097208c047aa51863ac5ffa1969062
iced-espresso <csu2018@gmail.com>
2022-08-22 21:27:41 +0900

    - Session을 jdbc에 남기도록 세팅 - SessionUser에 Login time 기록 - mustache 변경

--- src/main/java/com/espresso/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/espresso/springboot/config/auth/dto/SessionUser.java
@@ -4,15 +4,19 @@ import com.espresso.springboot.domain.user.User;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
+
+    private LocalDateTime localDateTime;
     public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
+        this.localDateTime = LocalDateTime.now();
     }
 }
--- src/main/java/com/espresso/springboot/web/IndexController.java
+++ src/main/java/com/espresso/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.espresso.springboot.web;
 
+import com.espresso.springboot.config.auth.dto.SessionUser;
 import com.espresso.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+        if (sessionUser != null){
+            model.addAttribute("name", sessionUser.getName());
+            model.addAttribute("loginTime", sessionUser.getLocalDateTime());
+        }
         return "index";
     }
 
@@ -24,9 +32,19 @@ public class IndexController {
         return "posts-save";
     }
 
+    @GetMapping("/access-denied")
+    public String accessDenied(){
+        return "access-denied";
+    }
+
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model){
         model.addAttribute("post", postsService.findById(id));
         return "posts-update";
+    }
+
+    @GetMapping("/local-login")
+    public String localLogin(){
+        return "local-login";
     }
 }
